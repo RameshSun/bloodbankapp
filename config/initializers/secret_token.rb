5c3d87efe5212b70263c84d1147c3c48569442e2
Ramesh Sundararaman <mesh.raman@gmail.com>
2012-04-26 13:39:31 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloodbankapp::Application.config.secret_token = '19945edc55c6d5e01187226e067a437496e405c97cf9aa6360e59f5250a937e50754d741cee109733a5bad63c3ca7ef922808e5a3deb1affe4e5a326d71fcd31'
